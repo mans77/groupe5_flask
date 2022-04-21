cff79b560dc6952041bd2a4fbe2a46a08a51e0ed
mans7 <sorogueye@gmail.com>
2022-04-21 07:25:33 +0000

    first commit

---
+++ website/auth.py
@@ -0,0 +1,25 @@
+from flask import Blueprint,render_template, request,flash, redirect, url_for
+
+
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/posts')
+def posts():
+    
+    return render_template("posts.html")
+
+@auth.route("/albums")
+def albums():
+   
+    return render_template("albums.html")
+
+@auth.route("/todos")
+def todos():
+   
+    return  render_template("newalbum.html")
+
+@auth.route("/infos")
+def infos():
+   
+    return render_template("infos.html")
