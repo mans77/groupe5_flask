b27d1b5f90ff68b13811b3d9cdb37d66650a87ea
Metzo-ops <>
2022-04-21 17:57:26 +0000

    this is the message

--- website/auth.py
+++ website/auth.py
@@ -7,17 +7,17 @@ auth = Blueprint('auth', __name__)
 @auth.route('/posts')
 def posts():
     
-    return render_template("posts.html")
+    return render_template("multicolor.html")
 
 @auth.route("/albums")
 def albums():
    
-    return render_template("albums.html")
+    return render_template("triplepost.html")
 
 @auth.route("/todos")
 def todos():
    
-    return  render_template("newalbum.html")
+    return  render_template("post_title.html")
 
 @auth.route("/infos")
 def infos():
