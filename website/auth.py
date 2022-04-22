5bb947edd5b134972ef989d4bf2193425d9c2262
Metzo-ops <>
2022-04-22 08:21:05 +0000

    foo

--- website/auth.py
+++ website/auth.py
@@ -7,17 +7,17 @@ auth = Blueprint('auth', __name__)
 @auth.route('/posts')
 def posts():
     
-    return render_template("multicolor.html")
+    return render_template("posts.html")
 
 @auth.route("/albums")
 def albums():
    
-    return render_template("triplepost.html")
+    return render_template("albums.html")
 
 @auth.route("/todos")
 def todos():
    
-    return  render_template("post_title.html")
+    return  render_template("todos.html")
 
 @auth.route("/infos")
 def infos():
