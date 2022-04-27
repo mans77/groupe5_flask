b1094e64d0642807e89d97cba98a20c3080c4f70
mans7 <sorogueye@gmail.com>
2022-04-27 08:26:23 +0000

    phase terminal

--- website/__init__.py
+++ website/__init__.py
@@ -3,17 +3,15 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 
 db = SQLAlchemy()
-DB_NAME = 'database'
+DB_NAME = 'flask'
 
 def create_app():
 
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "groupe5"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:password@localhost/{DB_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:groupe5@localhost/flask'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     db.init_app(app)
-
-   
 
     from .views import views
     from .auth import auth
@@ -22,14 +20,10 @@ def create_app():
 
     app.register_blueprint(views, url_prefix ="/")
     app.register_blueprint(auth, url_prefix  ="/")
-
-   
     
-
     return app 
-def create_database(app):
-    if not path.exists("website/" + DB_NAME):
-        db.create_all(app = app)
-        print("Create Database!")
+
+        
+        
 
     
--- website/views.py
+++ website/views.py
@@ -6,6 +6,34 @@ views = Blueprint("views", __name__)
 @views.route("/", methods=["GET","POST"])
 def home():
      
-     return render_template('base.html')
+     if  request.method == "POST":
+          id = request.form.get('id')
+          name  = request.form.get('name')
+          username = request.form.get('Username')
+          email = request.form.get('email')
+          phone = request.form.get('phone')
+          website = request.form.get('website')
+          street = request.form.get('street')
+          suite = request.form.get('suite')
+          city = request.form.get('city')
+          zipcode = request.form.get('zipcode')
+          lng = request.form.get('lng')
+          lat = request.form.get('lat')
+          name_company = request.form.get('compname')
+          catchPrase = request.form.get('catchPhrase')
+          bs = request.form.get('bs')
+
+         
+          user_add = Users(name = name, username = username, email = email, 
+                           phone = phone, website = website, street = street,
+                           suite = suite, city = city, zipcode = zipcode, 
+                           lng = lng, lat = lat, name_company = name_company,
+                           catchPhrase = catchPhrase, bs = bs)
+          db.session.add(user_add)
+          db.session.commit()
+          flash("Note added ", category="success")
+
+     return render_template('home.html')
+
 
 
